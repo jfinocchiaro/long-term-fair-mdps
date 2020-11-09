2d9edd5d71a52670f086d800afff82a38acbfc11
jfinocchiaro <22jjf22@gmail.com>
2020-11-09 07:44:50 -0700

    Responding to comments in political-influence.py

--- political-influence/players.py
+++ political-influence/players.py
@@ -4,11 +4,20 @@ import scipy.stats as stats
 #MDP class
 class players:
 
-    def __init__(self, group=1, shared=False, article=0, clickprob=0.5, clickprobparams=[0.5, 0.1, 0, 1]):
+    def __init__(self, group=1, shared=False, article=0, clicked = 0): #, clickprob=0.5 , clickprobparams=[0.5, 0.1, 0, 1]):
         self.group = group
         self.shared = shared
         self.article = article
-        self.clickprob = calcclickprob(*clickprobparams)
+        self.clicked = clicked
+        #self.clickprob = calcclickprob(*clickprobparams)
 
+'''
 def calcclickprob(mu, sigma, upper, lower):
     return stats.truncnorm((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+'''
+
+def calcclick(pga, pgb, thetag, v=1.0, c=1.0):
+	if pga * thetag + pgb * (1-thetag) > float(v/c):
+		return 1
+	else:
+		return -1 
--- political-influence/political-influence.py
+++ political-influence/political-influence.py
@@ -14,21 +14,28 @@ import platform_opt
 #cppB = [mu, sigma, upper, lower]
 
 # model variables
-T = 100
+T = 100 #number of timesteps
 M = 200 # M >= T
-M_a, M_b = 100, 100
+M_a, M_b = 100, 100 #number of members in groups a and b
 
 ######### questions and comments #########
-# Not a question, rather comment. Great job with the comments
-# - Is it necessary for number of users to be of the same quantity?
+# Not a question, rather comment. Great job with the comments 
+#	- ahh thanks :)
+# - Is it necessary for number of users to be of the same quantity? 
+# 	- no it is not.  They are currently represented by M_a and M_b above
 # - From what I understand, users are either 1 or -1, right? It's a bit confusing, since it's like a label ish, I think the same applies to the group.
+#	- I'm not sure what you mean by the first question.  The users are objects defined in players.py, and the attributes they havve, including "group", "clicked", and "shared" are appl +/- 1
 # - I would like to clarify if PAa + PAb = 1 or if PAa + PBa = 1
+# 	- great question: actually, neither.  PAb is the probability of a member of group A sharing article b given that they click on it.  Since these are independent, neither has a "sum to one" constraint.
 # - Here in the code, there is still probability of clicking, shouldn't it be based on the payoff?  
+# 	- Yes, good catch- I think I forgot to remove it, but believe it's not being used.
 # I however, I am uncertain about the how we will estimate cost, c and value, v. Is the c the same for all agents in a group? Is it 
 # lower in one group and higher for another? Does it change over time? And the v, how is estimated?
+# 	- my understanding was that c should be a function of group membership and article type, and same with v.  This just got discussed in the last meeting though, and figured I would keep it simple for testing now.  Can definitely change it though.
 # - In the overleaf, they seemed to say that unit mass is 1, I am not sure how this value is estimated and why it's deemed a constant. 
+# 	- The main goal is to be able to have a "handoff" of articles from one agent to some "next" agent.  The unit mass is 1 really just means that it's the same number of users at each timestep.
 # I however, I am certain it has an effect on the model. My suggestion would be to leave it arbitrary so that with it can change over time.
-# - 
+# 	- would be worth discussing in today's meeting! 
 
 
 P = {('1', '1') : 0.8, ('1', '-1'): 0.01,('-1', '1'):  0.05,('-1', '-1'):  0.2} #indexed (user group, article group).  probability of like | click, user group, article group
