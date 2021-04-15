1a9ae02479e6dd51eb1e70493f6c7847613f9e33
jfinocchiaro <22jjf22@gmail.com>
2021-04-15 15:09:11 -0600

    Refactoring in new file.  Data processing is different, but hopefully easier.

---
+++ political-influence-refactored/platform_opt.py
@@ -0,0 +1,142 @@
+import numpy as np
+import cvxpy as cp
+from scipy.special import betainc
+
+def psi(c, v, F):
+    ps = {}
+    for g in [-1,1]:
+        for s in [-1,1]:
+            alpha, beta = F[(s,g)]
+            c_ind = c[(s,g)]
+            v_ind = v[(s,g)]
+            #int from c_gs/v_gs to 1 of p dF_g,s(p), i.e. Probability of click
+            ps[(g,s)] =  1 - betainc(alpha, beta, float(c_ind) / v_ind)
+    
+    return ps
+
+#currently done by recursion; seems to be more efficient in closed form, but want to confirm it is correct before I switch code.
+def l(g,s,t, pi, theta, q, c,v,F):
+    ps = psi(c,v,F)
+    ti = (g+1) / 2
+    if t >= 2:
+        return ((q[g] * l(g,s,t-1, pi, theta, q,c,v,F)) + (1 - q[-g]) * l(-g,s,t-1, pi, theta, q,c,v,F)) * ps[(g,s)]
+    else:
+        if s == -1:
+            return pi[g] * theta[ti] * ps[(g,-1)]
+        if s == 1:
+            return pi[g] * (1 - theta[ti]) * ps[(g,1)]
+
+        
+# utility the platform has for user g clicking on article s        
+# indexed by (article, user)        
+unit_util = {(1,1)   : 1.,
+             (1,-1)  : 1.,
+             (-1,1)  : 1.,
+             (-1,-1) : 1.}
+
+def opt_additive(pi, q, T, epsilon,c,v,F, exposure_e = 0.0, u=unit_util):
+    
+    '''
+    params:
+    pi      : dictionary: proportion of users in each group
+    q       : dictionary: homophily variable
+    T       : int: number of total time steps (max)
+    epsilon : double: fairness violation allowed
+    c       : dictionary indexed (g,s): cost for clicking by group and article
+    v       : dictionary indexed (g,s): value for sharing by group and article
+    F       : dictionary indexed (g,s): alpha and beta parameters for beta distribution
+    u       : dictionary with platform's utility for a click.  can be thought of as a price charged.
+    '''
+
+    
+    #varaible theta_A, theta_B
+    theta = cp.Variable(2)
+    objective = cp.Maximize(cp.sum([u[(1,1)] * l(1,1,t, pi, theta, q, c,v,F) + u[(1,-1)] * l(-1,1,t,pi, theta, q, c,v,F) + u[(-1,1)] * l(1,-1,t, pi, theta, q, c,v,F) + u[(-1,-1)] *  l(-1,-1,t, pi, theta, q, c,v,F) for t in range(T)]))
+    constraints_theta = [exposure_e <= theta[0], theta[0] <= 1 - exposure_e, exposure_e <= theta[1], theta[1] <= 1 - exposure_e]
+    
+    #generate eta, used as constraints.
+    eta = {}
+    for s in [-1,1]:
+        for g in [-1,1]:
+            ti = (g+1) / 2 #theta index.... moving from -1 to 0 and  1 to 1
+            if s == 1:
+                eta[(s,g)] = pi[g] * theta[ti] + sum([ l(g,s,t, pi, theta,q,c,v,F) * q[g] + l(-g,s,t, pi, theta,q,c,v,F) * (1 - q[-g]) for t in range(1,T)])
+            else:
+                eta[(s,g)] = pi[g] * (1-theta[ti]) + sum([ l(g,s,t, pi, theta,q,c,v,F) * q[g] + l(-g,s,t, pi, theta,q,c,v,F) * (1 - q[-g]) for t in range(1,T)])
+                
+    
+    
+    constraints_eta = [eta[(1,1)] - eta[(-1,-1)] <= epsilon, eta[(-1,-1)] - eta[(1,1)] <= epsilon]
+    
+    
+    prob = cp.Problem(objective, constraints_theta+constraints_eta)
+    prob.solve()
+    th = {}
+    
+    th[1] = max(min(theta.value[1], 1.), 0.)
+    th[-1] = max(min(theta.value[0], 1.), 0.)
+    return th
+
+
+def opt_unconstrained(pi, q, T, epsilon,c,v,F, u=unit_util):
+    
+    '''
+    params:
+    pi      : dictionary: proportion of users in each group
+    q       : dictionary: homophily variable
+    T       : int: number of total time steps (max)
+    epsilon : double: fairness violation allowed
+    c       : dictionary indexed (g,s): cost for clicking by group and article
+    v       : dictionary indexed (g,s): value for sharing by group and article
+    F       : dictionary indexed (g,s): alpha and beta parameters for beta distribution
+    '''
+
+    #A is 1, B is -1
+    #varaible theta_A, theta_B
+    theta = cp.Variable(2)
+    objective = cp.Maximize(cp.sum([u[(1,1)] * l(1,1,t, pi, theta, q, c,v,F) + u[(-1,1)] * l(-1,1,t,pi, theta, q, c,v,F) + u[(1,-1)] * l(1,-1,t, pi, theta, q, c,v,F) + u[(-1,-1)] *  l(-1,-1,t, pi, theta, q, c,v,F) for t in range(T)]))
+    constraints_theta = [0 <= theta[0], theta[0] <= 1, 0 <= theta[1], theta[1] <= 1]
+    
+    prob = cp.Problem(objective, constraints_theta)
+    prob.solve()
+    th = {}
+    th[1] = max(min(theta.value[1], 1.), 0.)
+    th[-1] = max(min(theta.value[0], 1.), 0.)
+    return th
+
+
+def opt_ratios(pi, q, T,c,v,F, delta_low = 0.5, delta_high=2, exposure_e = 0.0, u=unit_util):
+    
+    '''
+    params:
+    pi      : dictionary: proportion of users in each group
+    q       : dictionary: homophily variable
+    T       : int: number of total time steps (max)
+    epsilon : double: fairness violation allowed
+    c       : dictionary indexed (g,s): cost for clicking by group and article
+    v       : dictionary indexed (g,s): value for sharing by group and article
+    F       : dictionary indexed (g,s): alpha and beta parameters for beta distribution
+    u       : dictionary with platform's utility for a click.  can be thought of as a price charged.
+    '''
+
+    
+    #varaible theta_A, theta_B
+    theta = cp.Variable(2)
+    objective = cp.Maximize(cp.sum([u[(1,1)] * l(1,1,t, pi, theta, q, c,v,F) + u[(1,-1)] * l(-1,1,t,pi, theta, q, c,v,F) + u[(-1,1)] * l(1,-1,t, pi, theta, q, c,v,F) + u[(-1,-1)] *  l(-1,-1,t, pi, theta, q, c,v,F) for t in range(T)]))
+    constraints_theta = [exposure_e <= theta[0], theta[0] <= 1 - exposure_e, exposure_e <= theta[1], theta[1] <= 1 - exposure_e]
+    
+    constraints_ratio = []
+    constraints_ratio.append(delta_low * sum([l(-1,-1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)]) <= sum([l(1,1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)]))
+    constraints_ratio.append(sum([l(1,1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)]) <= delta_high * sum([l(-1,-1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)]))
+    constraints_ratio.append(delta_low *  sum([l(-1,1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)])<= sum([l(1,-1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)]))
+    constraints_ratio.append(sum([l(1,-1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)])<= delta_high * sum([l(-1,1,t,  pi, theta, q, c,v,F) for t in range(1,T+1)]))
+
+    
+    
+    prob = cp.Problem(objective, constraints_theta+constraints_ratio)
+    prob.solve()
+    th = {}
+    
+    th[1] = max(min(theta.value[1], 1.), 0.)
+    th[-1] = max(min(theta.value[0], 1.), 0.)
+    return th
---
+++ political-influence-refactored/players.py
@@ -0,0 +1,62 @@
+import numpy as np
+import scipy.stats as stats
+
+#MDP class
+class Player:
+
+    def __init__(self, group=1, shared=False, article=0, clicked = 0): #, clickprob=0.5 , clickprobparams=[0.5, 0.1, 0, 1]):
+        self.group = group
+        self.shared = shared
+        self.article = article
+        self.clicked = clicked
+        #self.clickprob = calcclickprob(*clickprobparams)
+
+def calcclick(player, t, P, q, theta, c = 1, v=1):
+    g = player.group
+    s = player.article
+    #print(theta)
+    thetag = theta[g]
+    if t > 1:
+        return (q[g] * calcclick(player, t-1, P, q, theta, c, v) * P[(s,g)]) + ((1 - q[ -1 * g]) * P[(s,g)] * calcclick(player, t-1, P, q, theta, c, v))
+    if t == 1:
+        p =  P[(1,g)] * thetag + P[(-1,g)] * (1-thetag)
+        #print('p: ' + str(p) + ' c/v: ' + str(float(c/v)))
+        if p >= float(c/v):
+            return 1
+        else:
+            return 0
+        
+def calcclickdict(player, t, P, q, theta, c, v):
+    g = player.group
+    s = player.article
+    #print(theta)
+    thetag = theta[g]
+    if t > 1:
+        return (q[g] * calcclick(player, t-1, P, q, theta, c, v) * P[(s,g)]) + ((1 - q[ -1 * g]) * P[(s,g)] * calcclick(player, t-1, P, q, theta, c, v))
+    if t == 1:
+        p =  P[(1,g)] * thetag * (v[(1,g)] - c[(1,g)]) + P[(-1,g)] * (1-thetag) * (v[(-1,g)] - c[(1,g)])
+        #print('p: ' + str(p) + ' c/v: ' + str(float(c/v)))
+        if p >= 0.:
+            return 1
+        else:
+            return 0
+'''
+def calcclickprob(mu, sigma, upper, lower):
+    return stats.truncnorm((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+'''
+
+'''
+def calcclick(pga, pgb, thetag, v=1.0, c=1.0):
+	if pga * thetag + pgb * (1-thetag) > float(c/v):
+		return 1
+	else:
+		return -1 
+'''
+    
+
+    
+def coin_toss(p):
+    '''Mechanism to decide between A (-1) or B (1).'''
+    return -(2 * np.random.binomial(1, p) - 1)
+
+
